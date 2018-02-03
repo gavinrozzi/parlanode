c5a58c3acc03dbfd8d145f5021fa23227c455a6e
Matic Prevolsek <prevolsek@gmail.com>
2018-02-03 19:51:09 +0100

    Poslanska vprašanja in pobude P

--- cards/p/poslanska-vprasanja-in-pobude/card.vue
+++ cards/p/poslanska-vprasanja-in-pobude/card.vue
@@ -1,41 +1,72 @@
 <template>
-  <card-wrapper
-    :id="$options.cardData.cardData._id"
-    content-class="full"
-    :card-url="url"
-    :header-config="headerConfig">
+  <div class="card-scroll">
+    <card-wrapper
+      :id="$options.cardData.cardData._id"
+      :card-url="url"
+      :header-config="headerConfig">
 
-    <div slot="info">
-      <p class="info-text lead">Pregled osnovnih informacij poslanske skupine.</p>
-      <p class="info-text heading">METODOLOGIJA</p>
-      <p class="info-text">Vsebine za to kartico smo pridobili s spletnega mesta DZ RS (poslanska skupina, starost, članstva v delovnih telesih) in s spletnega mesta DVK (število prejetih glasov). Za ostale vsebine smo se obrnili na PR službe poslanskih skupin. Podatke so nam posredovali iz NSi, SD, (takrat še) ZaAB in ZL. SMC so nas usmerili na svoje spletno mesto, SDS pa na spletno mesto državnega zbora. Manjkajoče podatke smo pridobili s pomočjo iskalnika Google in jih za morebitne popravke pred objavo ponudili vsem poslanskim skupinam.</p>
-    </div>
+      <div slot="info">
+        <p class="info-text lead">Pregled osnovnih informacij poslanske skupine.</p>
+        <p class="info-text heading">METODOLOGIJA</p>
+        <p class="info-text">Vsebine za to kartico smo pridobili s spletnega mesta DZ RS (poslanska skupina, starost, članstva v delovnih telesih) in s spletnega mesta DVK (število prejetih glasov). Za ostale vsebine smo se obrnili na PR službe poslanskih skupin. Podatke so nam posredovali iz NSi, SD, (takrat še) ZaAB in ZL. SMC so nas usmerili na svoje spletno mesto, SDS pa na spletno mesto državnega zbora. Manjkajoče podatke smo pridobili s pomočjo iskalnika Google in jih za morebitne popravke pred objavo ponudili vsem poslanskim skupinam.</p>
+      </div>
 
-    <question-list :questionDays="data.results" />
-  </card-wrapper>
+      <question-list :questionDays="data.results" />
+    </card-wrapper>
+  </div>
 </template>
 
 <script>
 import common from 'mixins/common';
+import scroll from 'mixins/scroll';
 import { memberOverview } from 'mixins/contextUrls';
 import { memberTitle } from 'mixins/titles';
 import QuestionList from 'components/QuestionList.vue';
 
 export default {
   name: 'PoslanskaVprasanjaInPobudePoslanca',
-  mixins: [common, memberOverview, memberTitle],
+  mixins: [common, scroll, memberOverview, memberTitle],
   components: { QuestionList },
   data() {
     return {
       data: this.$options.cardData.data,
-      headerConfig: {
+    };
+  },
+  computed: {
+    headerConfig() {
+      return {
         circleIcon: 'og-list',
         heading: '&nbsp;',
         subheading: '7. sklic parlamenta',
         alternative: this.$options.cardData.cardData.altHeader === 'true',
         title: this.$options.cardData.cardData.name,
-      },
-    };
+        headerClass: 'filters--shadow',
+        headerClassApply: this.card.shouldShadow
+      }
+    },
+  },
+  mounted() {
+    this.card.shadowElement = 'questions';
+    if (document) {
+      document.getElementById(this.card.shadowElement).addEventListener('scroll', this.checkScrollPosition);
+    }
   },
 };
 </script>
+
+<style lang="scss">
+  .card-content {
+    padding: 0;
+
+    .card-scroll__wrapper {
+      margin: 0;
+    }
+
+    .date {
+      padding: 10px 20px;
+    }
+    ul {
+      margin: 0 20px !important;
+    }
+  }
+</style>
